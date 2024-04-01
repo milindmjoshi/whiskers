0d4b29c6874db871765cea9123328a8d643bfd96
Necromancer <9realmsstudio@gmail.com>
2024-04-01 18:58:01 -0400

    added my seed js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,55 @@
+//Seed that base boys
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Whiskey, Rating } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const userData = [
+  { username: 'john_doe' },
+  { username: 'jane_smith' },
+  { username: 'alex_williams' },
+];
+
+const whiskeyData = [
+  { name: 'Macallan 18', type: 'Scotch', origin: 'Scotland' },
+  { name: 'Buffalo Trace', type: 'Bourbon', origin: 'USA' },
+  { name: 'Jameson', type: 'Irish', origin: 'Ireland' },
+];
+
+const ratingData = [
+  { rating: 5 },
+  { rating: 4 },
+  { rating: 3 },
+];
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  try {
+    await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
     });
-  }
 
-  process.exit(0);
+    const whiskeys = await Whiskey.bulkCreate(whiskeyData, {
+      returning: true,
+    });
+
+    const ratings = await Rating.bulkCreate(ratingData, {
+      returning: true,
+    });
+
+    // Associate ratings with users and whiskeys
+    for (let i = 0; i < users.length; i++) {
+      await ratings[i].setUser(users[i]);
+      await ratings[i].setWhiskey(whiskeys[i]);
+    }
+
+    console.log('Database seeded successfully');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error seeding database:', err);
+    process.exit(1);
+  }
 };
 
 seedDatabase();
+
