21b3d9e290b9b8a79bfdcc630b43bb20d89f881c
Mark Barrie <mark.a.barrie@gmail.com>
2024-04-02 11:40:24 -0400

    home page styling version 1 complete

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,30 +2,19 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+    // Render the homepage template without dynamic data
+    res.render('homepage', {
+      // You can still pass the logged_in status if your layout requires it
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+module.exports = router;
 
 router.get('/project/:id', async (req, res) => {
   try {
