f348e725663559b1e9b2fcefad7f8b97d05db5a2
Mark Barrie <mark.a.barrie@gmail.com>
2024-04-02 16:19:27 -0400

    added search-result handlebars and temp search route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -82,4 +82,10 @@ router.get('/admin', (req, res) => {
   res.render('admin');
 });
 
+router.get('/search-results', (req, res) => {
+ 
+
+  res.render('search-results');
+});
+
 module.exports = router;
